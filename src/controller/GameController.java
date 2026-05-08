542f1782eeb5f30927be0ec1ce3ce4006574aa6b
Inder Grewal <inderdeepg8@gmail.com>
2026-05-07 22:20:19 -0700

    TM-20 Implement handleMove and handleAnswer methods (subject to refactor) to handle view to model interaction.

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Direction;
+import model.Door;
 
 public class GameController {
     private GameState myState;
@@ -18,25 +19,59 @@ public class GameController {
         if (theDir == null) {
             throw new IllegalArgumentException("Direction within handleMove must not be null.");
         }
+        Position newPos = myState.getCurrentPosition().translate(theDir);
+        Door door = myState.getMaze().getRoom(newPos).getDoor(theDir);
+
+        if (door.isOpen()) {
+            myState.setCurrentPosition(newPos);
+            myState.addVisitedRoom(newPos);
+            myState.firePropertyChange("PLAYER_MOVED", null, newPos); // TODO on GameState class
+        } else if (door.isLocked()) {
+            myState.setCurrentDirection(theDir);
+            myState.firePropertyChange("QUESTION_ASKED", null, door.getQuestion());
+        }
 
     }
 
     public void handleAnswer(final String theAnswer) {
-        if (theAnswer.equals("")) {
-            throw new IllegalArgumentException("Player answer must not be empty");
+        if (theAnswer.equals("") || theAnswer == null) {
+            throw new IllegalArgumentException("Player answer must not be blank or null.");
         }
+
+        Direction dir = myState.getCurrentDirection();
+        Position newPos = myState.getCurrentPosition().translate(dir);
+        Door door = myState.getMaze().getRoom(newPos).getDoor(dir);
+
+        if (door.attemptUnlock(theAnswer)) {
+            door.unlock();
+            myState.setCurrentPosition(newPos);
+            myState.addVisitedRoom(newPos);
+            myState.firePropertyChange("PLAYER_MOVED", null, newPos);
+            myState.firePropertyChange("ANSWER_RESULT", null, true);
+        } else {
+            door.block();
+            myState.firePropertyChange("ANSWER_RESULT", null, false);
+            myState.firePropertyChange("MAZE_UPDATED", null, null);
+        }
+
+        if (!myState.getMaze().isPathAvailable(myState.getCurrentPosition())) {
+            myState.firePropertyChange("GAME_OVER", null, GameStatus.LOST); // TODO build Stats ENUM for game update per event
+        }
+        
     }
 
     public void newGame(final int theWidth, final int theHeight) {
         if (theWidth < 0 || theHeight < 0) {
             throw new IllegalArgumentException("Maze width/height must be non-negative.");
         }
+        myState = new GameState(theWidth, theHeight);
     }
 
     public void saveGame(final String theFileName) {
         if (theFileName.equals("")) {
             throw new IllegalArgumentException("Save file name must not be empty.");
         }
+        myState.saveGame(theFileName);
     }
 
     public void loadGame(final String theFileName) {
